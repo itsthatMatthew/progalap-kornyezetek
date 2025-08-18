4875be4138253af49e1d68cf79c0d9f1b588f922
itsthatMatthew <csizmadiamate22@gmail.com>
2025-08-18 19:04:42 +0000

    feat: példakód

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int ominous_func(int x, int y) {
+  x *= y = 2;
+  y <<= x;
+  x = y * 42;
+  return x % 10;
+}
+
+int main() {
+  int a = 42, b = 0;
+
+  printf("Hello world, from a generic C program!"
+    "\nMy main contains two variables: a = %d and b = %d :3",
+    a, b);
+
+  if (a > b) {
+    puts("Woo-hoo, I can do some debugging on my own!"
+      "\n\nOr can I? *Vsauce music starts playing*\n");
+
+    b = ominous_func(a, b);
+  }
+  
+  for (int idx = 0; idx < b; ++idx) {
+    printf("[%d] Woah, it's starting to feel a tad dizzy...\n", idx);
+  }
+
+  puts("\nBai bai!!\n");
+
+  return 0;
+}
